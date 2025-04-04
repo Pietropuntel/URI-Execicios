5f54f7015cff5c71a7be40516b3f13f217586da2
Pietro Puntel <pietrosamueldossantospuntel@gmail.com>
2025-04-03 21:55:11 -0300

    imcompleto

---
+++ exercicio1001/exercicio1001/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+double A, B, calculo;
+A = double.Parse((Console.ReadLine()), CultureInfo.InvariantCulture);
+B = double.Parse((Console.ReadLine()), CultureInfo.InvariantCulture);
+
+calculo = double.Parse((Console.ReadLine());
+calculo = A + B;
+Console.WriteLine("A"+)
+
+
